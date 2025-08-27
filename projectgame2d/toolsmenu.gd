fc66f6966a7f3ac544e83fc28101def3e1e6d799
natchapolsr-ops <natchapol.sr@kkumail.com>
2025-08-27 21:51:01 +0700

    Player

---
+++ projectgame2d/toolsmenu.gd
@@ -0,0 +1,9 @@
+extends PanelContainer
+
+@onready var shovel: Button = $MarginContainer/HBoxContainer/Shovel
+@onready var axe: Button = $MarginContainer/HBoxContainer/Axe
+@onready var watering: Button = $MarginContainer/HBoxContainer/Watering
+@onready var plant: Button = $MarginContainer/HBoxContainer/Plant
+
+func _on_shovel_pressed() -> void:
+	pass # Replace with function body.
