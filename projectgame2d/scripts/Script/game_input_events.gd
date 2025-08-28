a0d2750a33590cc81b3985cf7dd6322c5953882e
natchapolsr-ops <natchapol.sr@kkumail.com>
2025-08-29 01:15:41 +0700

    Player

--- projectgame2d/scripts/Script/game_input_events.gd
+++ projectgame2d/scripts/Script/game_input_events.gd
@@ -1,1 +1,22 @@
-extends Node
+class_name GameInputEvents
+
+static var direction: Vector2
+static func movement_input() -> Vector2:
+	if Input.is_action_just_pressed("left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_just_pressed("right"):
+		direction = Vector2.RIGHT
+	elif Input.is_action_just_pressed("up"):
+		direction = Vector2.UP
+	elif Input.is_action_just_pressed("down"):
+		direction = Vector2.DOWN
+	else:
+		direction = Vector2.ZERO
+	
+	return direction
+
+static func is_movement_input() -> bool:
+	if direction == Vector2.ZERO:
+		return false
+	else:
+		return true
--- projectgame2d/scripts/Script/walk_state.gd
+++ projectgame2d/scripts/Script/walk_state.gd
@@ -1,17 +1,14 @@
 extends NodeState
 
-@export var Player:CharacterBody2D
-@export var animated_sprite:AnimatedSprite2D
 
-var direction:Vector2
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	direction = GameInputEvents.movement_input()
-		
-	animated_sprite.play("idle")		
+	pass
+
+
 func _on_next_transitions() -> void:
 	pass
 
