5e5957a89806c0b66b9c7f75e4bd9f350da28cda
natchapolsr-ops <natchapol.sr@kkumail.com>
2025-08-28 18:12:28 +0700

    Player

---
+++ projectgame2d/scripts/Script/idle_state.gd
@@ -0,0 +1,31 @@
+extends NodeState
+
+@export var Player:CharacterBody2D
+@export var animated_sprite:AnimatedSprite2D
+
+var direction:Vector2
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	if Input.is_action_just_pressed("left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_just_pressed("right"):
+		direction = Vector2.RIGHT
+	elif Input.is_action_just_pressed("up"):
+		direction = Vector2.UP
+	elif Input.is_action_just_pressed("down"):
+		direction = Vector2.DOWN
+	else:
+		direction = Vector2.ZERO		
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
