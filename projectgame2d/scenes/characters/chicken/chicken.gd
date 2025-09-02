4eec47f3f0c51789a0426afde23bd63a05dc73e0
P0kkalaT <phakawwut.p@kkumail.com>
2025-09-02 16:51:41 +0700

    fix

--- projectgame2d/scenes/characters/chicken/chicken.gd
+++
@@ -1,5 +0,0 @@
-extends NonPlayableCharacter
-
-
-func _ready() -> void:
-	walk_cycles = randi_range(min_walk_cycle, max_walk_cycle)
--- projectgame2d/scenes/characters/non_playable_character.gd
+++
@@ -1,8 +0,0 @@
-class_name NonPlayableCharacter
-extends CharacterBody2D
-
-@export var min_walk_cycle: int = 2
-@export var max_walk_cycle: int = 6
-
-var walk_cycles: int
-var current_walk_cycle: int
