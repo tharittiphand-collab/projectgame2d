18f141ad3ad92e7077a99a08918e6e3dca597c92
P0kkalaT <phakawwut.p@kkumail.com>
2025-09-03 00:38:56 +0700

    Player_state

--- scripts/game_input_events.gd
+++ scripts/game_input_events.gd
@@ -1,20 +1,22 @@
 class_name GameInputEvents
 
 static var direction: Vector2
+
 static func movement_input() -> Vector2:
-	if Input.is_action_just_pressed("left"):
+	if Input.is_action_pressed("walk_left"):
 		direction = Vector2.LEFT
-	elif Input.is_action_just_pressed("right"):
+	elif Input.is_action_pressed("walk_right"):
 		direction = Vector2.RIGHT
-	elif Input.is_action_just_pressed("up"):
+	elif Input.is_action_pressed("walk_up"):
 		direction = Vector2.UP
-	elif Input.is_action_just_pressed("down"):
+	elif Input.is_action_pressed("walk_down"):
 		direction = Vector2.DOWN
 	else:
 		direction = Vector2.ZERO
-	
+		
 	return direction
-
+	
+	
 static func is_movement_input() -> bool:
 	if direction == Vector2.ZERO:
 		return false
@@ -22,6 +24,6 @@ static func is_movement_input() -> bool:
 		return true
 		
 static func use_tool() -> bool:
-	var use_tool_value : bool = Input.is_action_just_pressed("hit") 
+	var use_tool_value: bool = Input.is_action_just_pressed("hit")
 	
-	return use_tool_value		
+	return use_tool_value
--- scripts/state_machine/node_state_machine.gd
+++ scripts/state_machine/node_state_machine.gd
@@ -31,7 +31,7 @@ func _physics_process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_physics_process(delta)
 		current_node_state._on_next_transitions()
-		#print(parent_node_name, " Current State: ", current_node_state_name)
+		#print(parent_noddwdsae_name, " Current State: ", current_node_state_name)
 
 func transition_to(node_state_name : String) -> void:
 	if node_state_name == current_node_state.name.to_lower():
@@ -49,4 +49,4 @@ func transition_to(node_state_name : String) -> void:
 	
 	current_node_state = new_node_state
 	current_node_state_name = current_node_state.name.to_lower()
-	#print("Current State: ", current_node_state_name)
+	print("Current State: ", current_node_state_name)
