76fc01227de5808bc3a6d8039d004de063f325b5
tharittiphand-collab <tharittiphan.d@kkumail.com>
2025-09-02 16:14:40 +0700

    scene

---
+++ projectgame2d/scenes/components/hit_component.gd
@@ -0,0 +1,5 @@
+class_name HitComponet
+extends Area2D
+
+@export var current_tool : DataTypes.Tools = DataTypes.Tools.None
+@export hit_damage : int = 1
---
+++ projectgame2d/scenes/components/hurt_component.gd
@@ -0,0 +1,10 @@
+class_name HurtComponet
+extends Area2D
+
+@export var tool : DataTypes.Tools = DataTypes.Tools.None
+
+
+signal on_hurt
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
