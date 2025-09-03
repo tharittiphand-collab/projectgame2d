72f862d1a9c03cdbf9e5988e4b3bdd092bfa291f
P0kkalaT <phakawwut.p@kkumail.com>
2025-09-04 06:34:36 +0700

    final

--- scenes/Ui/tools_panel.gd
+++ scenes/Ui/tools_panel.gd
@@ -1,23 +1,33 @@
 extends PanelContainer
-@onready var tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
-@onready var Axe: Button = $MarginContainer/HBoxContainer/ToolAxe
-@onready var watering: Button = $MarginContainer/HBoxContainer/Watering
-@onready var plant: Button = $MarginContainer/HBoxContainer/Plant
+
+@onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
+@onready var tool_tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
+@onready var tool_watering_can: Button = $MarginContainer/HBoxContainer/ToolWateringCan
+@onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
+@onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
+
 
 func _on_tool_axe_pressed() -> void:
-	ToolManager.select_tool(DataTypes.Tools.Axe)
-
+	ToolManager.select_tool(DataTypes.Tools.AxeWood)
 
 func _on_tool_tilling_pressed() -> void:
-	ToolManager.select_tool(DataTypes.Tools.Tillground)
+	ToolManager.select_tool(DataTypes.Tools.TillGround)
 
+func _on_tool_watering_can_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.WaterCrops)
 
-func _on_watering_pressed() -> void:
-	ToolManager.select_tool(DataTypes.Tools.Watering)
+func _on_tool_corn_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantCorn)
 
+func _on_tool_tomato_pressed() -> void:
+	ToolManager.select_tool(DataTypes.Tools.PlantTomato)
 
-func _on_corn_pressed() -> void:
-	ToolManager.select_tool(DataTypes.Tools.Corn)
-	
-func _on_tomato_pressed() -> void:
-	ToolManager.select_tool(DataTypes.Tools.Tomato)
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("release_tool"):
+		ToolManager.select_tool(DataTypes.Tools.None)
+		tool_axe.release_focus()
+		tool_tilling.release_focus()
+		tool_watering_can.release_focus()
+		tool_corn.release_focus()
+		tool_tomato.release_focus()
+		
